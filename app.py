88ec6d52a2c673543946d198ac4848f119300ccf
Michael Dinh <mkdinh94@gmail.com>
2020-06-21 14:50:28 -0400

    add pymongo

--- app.py
+++ app.py
@@ -1,33 +1,53 @@
 from flask import Flask, render_template, jsonify
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 from os import environ
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or "sqlite:///notepad.sqlite"
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or "sqlite:///notepad.sqlite"
+# db = SQLAlchemy(app)
 
-db = SQLAlchemy(app)
+app.config['MONGO_URI'] = environ.get('MONDODB_URI') or "mongodb://localhost:27017/notepad"
+mongo = PyMongo(app)
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String)
+# class Task(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     description = db.Column(db.String)
 
 @app.route('/')
 def index():
     return render_template('index.html', name="Michael")
 
+
+# @app.route('/tasks')
+# def tasks():
+#     tasks = db.session.query(Task)
+#     data = []
+
+#     for task in tasks:
+#         item = {
+#             "id": task.id,
+#             "description": task.description
+#         }
+#         data.append(item)
+
+#     return jsonify(data)
+
 @app.route('/tasks')
 def tasks():
-    tasks = db.session.query(Task)
+    tasks = mongo.db.tasks.find()
     data = []
 
     for task in tasks:
+        print(task)
         item = {
-            "id": task.id,
-            "description": task.description
+            "id": str(task['_id']),
+            "description": task['description']
         }
         data.append(item)
 
     return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
