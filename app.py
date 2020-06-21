3254c04b446e105d1f94d47539ece4d7d76edc46
Michael Dinh <mkdinh94@gmail.com>
2020-06-21 13:55:11 -0400

    add sqlalchemy

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or "sqlite:///notepad.sqlite"
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
 
 @app.route('/')
 def index():
     return render_template('index.html', name="Michael")
+
+@app.route('/tasks')
+def tasks():
+    tasks = db.session.query(Task)
+    data = []
+
+    for task in tasks:
+        item = {
+            "id": task.id,
+            "description": task.description
+        }
+        data.append(item)
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
